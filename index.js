11c4b7a2272f83a33f8f5b21b9252019a99a325b
Michael Warren <mwarrn@amazon.com>
2022-05-08 00:57:46 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Add your code here
+function submitData(userName, userEmail) {
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail,
+        }),
+    };
+    
+    return fetch('http://localhost:3000/users', configurationObject)
+    .then((response) => response.json())
+    .then((user) => document.body.innerHTML = user.id)
+    .catch((error) => document.body.append(error.message));
+}
